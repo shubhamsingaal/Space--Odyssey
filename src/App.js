d9e9cf18ccba0e76fb76c8c19d1d881f2cceb3cf
Shubham Kumar <shubhamkraj200@gmail.com>
2023-06-09 11:26:41 +0530

    updated folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
 import Home from "./components/home"
 import Auth from "./components/auth"
-
+import Events from "./components/events"
+import Contact from "./components/contact"
 import NotFound from "./components/notfound"
 
 
@@ -10,9 +11,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/auth" element={<Auth />} />
-
+        <Route exact path="/events" element={<Events />} />
+        <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/404" element={<NotFound />} />
-
         <Route exact path="*" element={<Navigate to="/404" />} />
         <Route exact path="/" element={<Home />}></Route>
       </Routes>
